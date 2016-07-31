989faed2bf6bc5451171bba61884abb917b7b300
amazingandyyy <amazingandyyy@gmail.com>
2016-07-31 03:59:20 -0700

    adding fields to dashboard account card
    adding fields to dashboard account card

--- client/app/dashboard/account/account.component.ts
+++ client/app/dashboard/account/account.component.ts
@@ -6,19 +6,22 @@ import { User } from '../../shared/types/user'
 
 @Component({
     moduleId: module.id,
-    selector: 'sp-account',
+    selector: 'yeah-account',
     templateUrl: 'account.component.html',
     styleUrls: ['account.style.css']
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit{
     currentUser = {};
         
     constructor() {
-        console.log('check currentUser data', JSON.parse(localStorage.getItem('current_user')) );
-        this.currentUser = JSON.parse(localStorage.getItem('current_user'));
+        
     }
 
     createAt(unix) {
         return moment(unix).format('LLL');
+    }
+    ngOnInit(){
+        console.log('check currentUser data', JSON.parse(localStorage.getItem('current_user')) );
+        this.currentUser = JSON.parse(localStorage.getItem('current_user'));
     }
 }
