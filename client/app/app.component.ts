4ddaa749595152a67777434578422ac9a55a7c70
amazingandyyy <amazingandyyy@gmail.com>
2016-08-07 22:52:47 -0700

    clean up js/vendor
    clean up js/vendor

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
-import { Router }      from '@angular/router';
+import { Component }            from '@angular/core';
+import { ROUTER_DIRECTIVES }    from '@angular/router';
+import { Router }               from '@angular/router';
 
 @Component({
     moduleId: module.id,
