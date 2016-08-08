04895cd35b96f3aaec7a0afba48cb6c7dc750283
davidyu37 <davidyu37@gmail.com>
2016-08-09 02:15:44 +0800

    cannot find name module error

--- app.js
+++ app.js
@@ -90,10 +90,10 @@ server.on('error', function(err) {
     console.error(err)
 })
 // server.on('listening', )
-var io = require('socket.io')(server);
-io.on('connection', function(socket){
-  socket.on('event', function(data){});
-  socket.on('disconnect', function(){});
-});
+// var io = require('socket.io')(server);
+// io.on('connection', function(socket){
+//   socket.on('event', function(data){});
+//   socket.on('disconnect', function(){});
+// });
 
 module.exports = app;
