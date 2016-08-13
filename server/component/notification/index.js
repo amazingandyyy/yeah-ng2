cce835b9b3db150aff536775bc3e53f7008e71c5
davidyu37 <davidyu37@gmail.com>
2016-08-13 12:10:36 +0900

    create notification backend

--- server/component/notification/index.js
+++ server/component/notification/index.js
@@ -3,9 +3,12 @@
 var express = require('express');
 var router = express.Router();
 var Notification = require('./notification.model');
+var User = require('../user/models/user.model');
 var Controller = require('./notification.controller');
 
 
 router.get('/', Controller.index);
+
+router.post('/send', User.authMiddleware, Controller.createNotification);
 
 module.exports = router;
--- server/component/notification/notification.controller.js
+++ server/component/notification/notification.controller.js
@@ -5,3 +5,17 @@ var Notification = require('./notification.model');
 exports.index = function(req, res) {
 	res.render('index');
 };
+
+exports.createNotification = function(req, res) {
+	var sentFrom = { from: req.user._id };
+	var newNotice = _.merge(req.body, sentFrom);
+	Notification.create(newNotice, function(err, notice) {
+		if(err) { return handleError(res, err); }
+		return res.status(201).json(notice);
+	});
+};
+
+function handleError(res, err) {
+  console.log(err);
+  return res.status(500).send(err);
+}
