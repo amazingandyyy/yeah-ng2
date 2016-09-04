b7a71bce0be4e86b5c4e45ae15c22bbc42982f3f
amazingandyyy <amazingandyyy@gmail.com>
2016-09-04 10:36:05 -0700

    before getting updated to rc6
    before getting updated to rc6

--- server/component/notification/notification.model.js
+++ server/component/notification/notification.model.js
@@ -83,15 +83,18 @@ notificationSchema.statics = {
             }
         });
         console.log('notice: ', notice)
-        notice.save((err, savedNotice) => {
-            if (err) return cb(err)
-            console.log('savedNotice: ', savedNotice)
-            Notification.findById(savedNotice._id,(err, dbNotice)=>{
-                if (err) return cb(err)
-                console.log('dbNotice: ', dbNotice)
-                cb(null, dbNotice);
-            })
-        });
+        // -> die here
+        // notice.save((err, savedNotice) => {
+        //     console.log('checkkkk!')
+        //     if (err) return cb(err)
+        //     console.log('savedNotice: ', savedNotice)
+        //     Notification.findById(savedNotice._id,(err, dbNotice)=>{
+        //         if (err) return cb(err)
+        //         console.log('dbNotice: ', dbNotice)
+        //         cb(null, dbNotice);
+        //     })
+        // });
+        cb(null, 'testing');
     },
     getThreeNew: function (userId, cb) {
         Notification.find({ to: userId, 'read.state': false })
